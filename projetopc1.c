d4e631ed2e83c622fe781daa7be8590c960284ac
Ciro Moraes <133052976+ciromoraesr@users.noreply.github.com>
2023-09-16 13:43:13 -0300

    Update projetopc1.c
    arrumando o erro antigo que foi pensar na resolução desse problema com inteiros

--- projetopc1.c
+++ projetopc1.c
@@ -1,42 +1,18 @@
 #include <stdio.h>
 
-int oum(long long int a,long long int b){
-    return a + b;
+int itochar(int num){
+    return num - '0';
 }
-int odois(long long int a,long long int b){
-    return a - b;
-
+int chartoit(char num){
+    return num + '0';
 }
-int otres(long long int a,long long int b){
-    return a * b;
+void soma(){
+    
 }
 
-int main(){
-    int i, n;
-
-    scanf("%d", &n);
-    long long int v[n][3], result;
-    int o[n];
-    for (i = 0; i < n; i++){
-        for(int j = 0; j < 2; j++){
-            scanf("%lld", &v[i][j]);
-
-            }
-        scanf("%d", &o[i]);
-        }
-        printf("\n");
-    for(i = 0; i < n; i++){
-            if(o[i]==1){
-               result = oum(v[i][0], v[i][1]);
-            }
-            if(o[i]==2){
-                result = odois(v[i][0], v[i][1]);
-            }
-            if(o[i]==3){
-                result = otres(v[i][0], v[i][1]);
-
-            }
-        printf("%lld\n", result);
-    }
+int main()
+{
+  
     return 0;
 }
+
