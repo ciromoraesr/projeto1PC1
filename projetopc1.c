d877f8ec663235766139e8e459e9f7652d5a0fab
Ciro Moraes <133052976+ciromoraesr@users.noreply.github.com>
2023-09-19 11:03:16 -0300

    Update projetopc1.c
    adicionando a parte que dá o resultado negativo

--- projetopc1.c
+++ projetopc1.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 int chartoint(int num){
     return num - '0';
 }
-
 char intochar(int num){
     return num + '0';
 }
-
 void soma(char a[501], char b[501]){
     char temp[502], soma[1002];
-
     if (strlen(a) > strlen(b)){
         int diff = strlen(a) - strlen(b);
         for (int i = 0; i < diff; i++){
@@ -69,14 +67,45 @@ void soma(char a[501], char b[501]){
     for (int i = start; i < strlen(soma); i++){
         if (chartoint(soma[i])>= 0 && chartoint(soma[i])<= 9){
         printf("%c", soma[i]);
+            }
         }
     }
 }
+void sub(char a[501], char b[501]){
+    int lena = strlen(a), lenb = strlen(b);
+    char temp[1002], sub[1002];
+    int resulnegativo = 0;
+    int maior = 0;
+
+    if (lena > lenb){
+        maior = 1;
+
+    }else if(lenb > lena){
+        maior = 2;
+    }else{
+        for (int i = 0; i < lena; i++){
+            if (a[i] > b[i]){
+                maior = 1;
+                break;
+
+            }else{
+                maior = 2;
+                break;
+            }
+        }
+    }
+    if (maior == 2){
+        resulnegativo = 1;
+    }
+    else if(maior == 1){
+        resulnegativo = 0;
+    }
+
 }
 
 int main(){
-    char a[501] = "000000";
-    char b[501] = "0";
+    char a[501] = "127349847218943757471829";
+    char b[501] = "9942174127482147421798427421";
 
     soma(a, b);
     return 0;
