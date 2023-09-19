42fa3b6eab299195e7485e2b91bb047a0a89db51
Ciro Moraes <133052976+ciromoraesr@users.noreply.github.com>
2023-09-19 18:45:38 -0300

    Update projetopc1.c
    consegui implementar um pouquinho da parte de subtração

--- projetopc1.c
+++ projetopc1.c
@@ -77,6 +77,7 @@ void subr(char a[501], char b[501]){
     char temp[1002], sub[1002];
     int resulnegativo = 0;
     int maior = 0;
+    int carry = 0;
 
     if (lena > lenb){
         maior = 1;
@@ -121,14 +122,50 @@ void subr(char a[501], char b[501]){
             temp[i] = a[i - diff];
         }
         temp[lenb] = '\0';
+    
+
+    }else{
+       if (resulnegativo == 1){
+        for(int i = lena -1; i>= 0; i -- ){
+                int x = chartoint(b[i]);
+                int y = chartoint(a[i]);
+                int diff = x - y - carry;
+            
+            if (diff < 0){
+                diff = diff + 10;
+                carry = 1;
+            }else{
+                carry = 0;
+            }
+            sub[i] = intochar(diff);
+
+       }}else{
+        for(int i = lena -1; i>= 0; i -- ){
+                int x = chartoint(a[i]);
+                int y = chartoint(b[i]);
+                int diff = x - y - carry;
+            
+            if (diff < 0){
+                diff = diff + 10;
+                carry = 1;
+            }else{
+                carry = 0;
+            }
+            sub[i] = intochar(diff);
+       } 
     }
-
 }
-
+if (resulnegativo == 1){
+    printf("-");
+}
+for(int i = 0; i < lena; i++){
+    printf("%c", sub[i]);
+}
+}
 int main(){
-    char a[501] = "255";
-    char b[501] = "45";
+    char a[501] = "05";
+    char b[501] = "10";
 
-    soma(a, b);
+    subr(a, b);
     return 0;
 }
