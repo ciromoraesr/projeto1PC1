1746914d652c4496316487a125c2d776d26c70bc
Ciro Moraes <133052976+ciromoraesr@users.noreply.github.com>
2023-09-20 19:36:22 -0300

    trabalho quase completo
    só arrumar uns erros na subtração e comentar e otimizar algumas partes

--- projetopc1.c
+++ projetopc1.c
@@ -12,7 +12,6 @@ int checkzer(char s[501]){
             zero = 1;
             break;
         }
-        
     }
     return zero;
 }
@@ -29,6 +28,65 @@ void post(char x[501], char y[501], char tempo[501]) {
         tempo[i] = y[i - diffe];
     }
     tempo[lena] = '\0';
+}
+void soma(char a[501], char b[501]) {
+    char temp[502], soma[1002];
+    int lena = strlen(a), lenb = strlen(b);
+    if (lena > lenb) {
+        int diff = lena - lenb;
+        for (int i = 0; i < diff; i++) {
+            temp[i] = '0';
+        }
+        for (int i = diff; i < lena; i++) {
+            temp[i] = b[i - diff];
+        }
+        temp[lena] = '\0';
+        int carry = 0;
+        for (int i = strlen(temp) - 1; i >= 0; i--) {
+            int sum = chartoint(temp[i]) + chartoint(a[i]) + carry;
+            carry = sum / 10;
+            soma[i + 1] = intochar(sum % 10);
+        }
+        soma[0] = intochar(carry);
+    } else if (lenb > lena) {
+        int diff = lenb - lena;
+        for (int i = 0; i < diff; i++) {
+            temp[i] = '0';
+        }
+        for (int i = diff; i < lenb; i++) {
+            temp[i] = a[i - diff];
+        }
+        temp[lenb] = '\0';
+        int carry = 0;
+        for (int i = strlen(temp) - 1; i >= 0; i--) {
+            int sum = chartoint(temp[i]) + chartoint(b[i]) + carry;
+            carry = sum / 10;
+            soma[i + 1] = intochar(sum % 10);
+        }
+        soma[0] = intochar(carry);
+    } else {
+        int carry = 0;
+        for (int i = lena - 1; i >= 0; i--) {
+            int sum = chartoint(a[i]) + chartoint(b[i]) + carry;
+            carry = sum / 10;
+            soma[i + 1] = intochar(sum % 10);
+        }
+        soma[0] = intochar(carry);
+    }
+    if (atoi(soma) == 0) {
+        printf("0");
+    } else {
+        int start = 0;
+        while (soma[start] == '0' && start < strlen(soma)) {
+            start++;
+        }
+        for (int i = start; i < strlen(soma); i++) {
+            if (chartoint(soma[i]) >= 0 && chartoint(soma[i]) <= 9) {
+                printf("%c", soma[i]);
+            }
+        }
+    }
+    printf("\n");
 }
 void subr(char a[501], char b[501]) {
     //declarando as variáveis usadas
@@ -73,7 +131,6 @@ void subr(char a[501], char b[501]) {
     }
     else if(lenb > lena){
        post(b, a, temp);
-     
     //if (resulnegativo == 1){
         for(int i = lenb  - 1 ; i>= 0; i -- ){
                 int x = chartoint(b[i]);
@@ -88,7 +145,6 @@ void subr(char a[501], char b[501]) {
             }
             sub[i] = intochar(diff);
     }
-
     }else{
        if (resulnegativo == 1){
         for(int i = lena -1; i>= 0; i -- ){
@@ -103,7 +159,6 @@ void subr(char a[501], char b[501]) {
                 carry = 0;
             }
             sub[i] = intochar(diff);
-
        }}else{
         for(int i = lena -1; i>= 0; i -- ){
                 int x = chartoint(a[i]);
@@ -120,7 +175,6 @@ void subr(char a[501], char b[501]) {
        } 
     }
 }
-
 if (atoi(sub) == 0) {
         printf("0");
 } else {
@@ -147,27 +201,50 @@ printf("\n");
 void mult(char a[501], char b[501]){
     int lena = strlen(a), lenb = strlen(b);
     char temp[501], resultado[1002];
-    int carry = 0;
-    if (lena > lenb){
-        post(a, b, temp);
-
-
-
-
-    }else if(lenb > lena){
-        post(b, a, temp);
-
-
-
-
-   }else{
+        for (int i = 0; i < lena + lenb; i++) {
+        resultado[i] = '0';
+        }
+        resultado[lena + lenb] = '\0';
+        for(int i = lenb-1; i >= 0; i--){
+        int carry = 0;
+            for(int j = lena -1; j >= 0; j--){
+                int multi = (chartoint(b[i]) * chartoint(a[j]) + chartoint(resultado[i+j])+ carry);
+                resultado[j+ i +1] = intochar(multi%10);
+                carry = multi/ 10;   
+            }
+        resultado[i] = intochar(carry);
+        }
+    int h = checkzer(resultado);
+    int start  = 0;
+    while (resultado[start] == '0' && start < strlen(resultado)) {
+            start++;
+    }
+    if (h == 0){
+        printf("0");
+    }else{
     
-   }
+    for (int i = start; i < strlen(resultado); i++){
+         if (chartoint(resultado[i]) >= 0 && chartoint(resultado[i]) <= 9) {
+                printf("%c", resultado[i]);
+            }
+    }
+    }
+    printf("\n");
 }
 int main(){
-    char a[501] = "10000";
-    char b[501] = "1000";
-
-    subr(a, b);
+    char a[501];
+    char b[501];
+    int n, o;
+    scanf("%d", &n);
+    for(int i =0; i < n; i++){
+        scanf("%s %s %d", a, b, &o);
+        if (o == 1){
+            soma(a, b);
+        }else if(o == 2){
+            subr(a, b);
+        }else{
+            mult(a, b);
+        }
+    }
     return 0;
 }
